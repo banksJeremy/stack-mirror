f0203e441bd6f20c96bba5f1bab0f32f052257e7
Jeremy Banks <jeremy@jeremybanks.ca>
2011-12-06 01:53:18 -0500

    m

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE TABLE IF NOT EXISTS Snapshots (
     Name                  TEXT PRIMARY KEY --- precedence is determined by binary sort order.
 );
 
-INSERT OR IGNORE INTO Snapshots (Id, Name) VALUES (1000, "~preload");
+INSERT OR IGNORE INTO Snapshots (Name) VALUES ("~preload");
 
 CREATE TABLE IF NOT EXISTS Users (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     Reputation            INTEGER,
     CreationDate          NUMERIC,
     DisplayName           TEXT,
@@ -24,25 +24,25 @@ CREATE TABLE IF NOT EXISTS Users (
     Age                   INTEGER
 );
 
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
-                     VALUES (  -1,     "~preload"     "Community",   1217462400);
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
+                     VALUES (  -1,     "~preload",    "Community",   1217462400);
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
                      VALUES (   1,     "~preload",  "Jeff Atwood",   1217514151);
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
                      VALUES (   2,     "~preload", "Geoff Dalgas",   1217514151);
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
                      VALUES (   3,     "~preload", "Jarrod Dixon",   1217514151);
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
                      VALUES (   4,     "~preload", "Joel Spolsky",   1217514151);
-INSERT OR IGNORE INTO Users (  Id, LastSnapshotId,    DisplayName, CreationDate)
+INSERT OR IGNORE INTO Users (  Id, LastSnapshotName,    DisplayName, CreationDate)
                      VALUES (1114,     "~preload", "Jeremy Banks",   1218545682);
 
 --- Early dumps don't include the Id for badges. Since badges are practically never deleted,
 --- we'll just discard these records.
 CREATE TABLE IF NOT EXISTS Badges (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     UserId                INTEGER NOT NULL     REFERENCES Users,
     Name                  TEXT    NOT NULL,
     Date                  NUMERIC
@@ -51,23 +51,23 @@ CREATE TABLE IF NOT EXISTS Badges (
 
 CREATE TABLE IF NOT EXISTS PostTypes (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     Name                  TEXT    NOT NULL
 );
 
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (1, "~preload", "Question");
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (2, "~preload", "Answer");
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (3, "~preload", "Tag Wiki Orphan");
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (4, "~preload", "Tag Wiki Excerpt");
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (5, "~preload", "Tag Wiki Body");
-INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotId, Name) VALUES (6, "~preload", "Nomination");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (1, "~preload", "Question");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (2, "~preload", "Answer");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (3, "~preload", "Tag Wiki Orphan");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (4, "~preload", "Tag Wiki Excerpt");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (5, "~preload", "Tag Wiki Body");
+INSERT OR IGNORE INTO PostTypes (Id, LastSnapshotName, Name) VALUES (6, "~preload", "Nomination");
 
 CREATE TABLE IF NOT EXISTS Posts (
     Id                    INTEGER PRIMARY KEY,
     PostTypeId            INTEGER NOT NULL     REFERENCES PostTypes,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     AcceptedAnswerId      INTEGER,
     ParentId              INTEGER              REFERENCES Posts,
     CreationDate          NUMERIC,
@@ -91,8 +91,8 @@ CREATE TABLE IF NOT EXISTS Posts (
 
 CREATE TABLE IF NOT EXISTS Comments (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     PostId                INTEGER NOT NULL     REFERENCES Posts,
     Score                 INTEGER,
     Text                  TEXT    NOT NULL,
@@ -104,58 +104,59 @@ CREATE TABLE IF NOT EXISTS Comments (
 
 CREATE TABLE IF NOT EXISTS PostHistoryTypes (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     Name                  TEXT NOT NULL
 );
 
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 1, "~preload", "Initial Title");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 2, "~preload", "Initial Body");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 3, "~preload", "Initial Tags");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 4, "~preload", "Edit Title");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 5, "~preload", "Edit Body");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 6, "~preload", "Edit Tags");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 7, "~preload", "Rollback Title");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 8, "~preload", "Rollback Body");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES ( 9, "~preload", "Rollback Tags");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (10, "~preload", "Post Closed");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (11, "~preload", "Post Reopened");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (12, "~preload", "Post Deleted");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (13, "~preload", "Post Undeleted");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (14, "~preload", "Post Locked");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (15, "~preload", "Post Unlocked");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (16, "~preload", "Community Owned");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (17, "~preload", "Post Migrated");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (18, "~preload", "Question Merged");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (19, "~preload", "Question Protected");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (20, "~preload", "Question Unprotected");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (21, "~preload", "Post Dissociated");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (22, "~preload", "Question Unmerged");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (23, "~preload", "(Unknown Event)");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (24, "~preload", "Suggested Edit Applied");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (25, "~preload", "Post Tweeted");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (26, "~preload", "Vote Deleted");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (27, "~preload", "Question Migrated (Silently)");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (28, "~preload", "(Unknown Event)");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (29, "~preload", "(Unknown Event)");
-INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotId, Name) VALUES (30, "~preload", "(Unknown Event)");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 1, "~preload", "Initial Title");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 2, "~preload", "Initial Body");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 3, "~preload", "Initial Tags");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 4, "~preload", "Edit Title");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 5, "~preload", "Edit Body");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 6, "~preload", "Edit Tags");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 7, "~preload", "Rollback Title");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 8, "~preload", "Rollback Body");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES ( 9, "~preload", "Rollback Tags");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (10, "~preload", "Post Closed");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (11, "~preload", "Post Reopened");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (12, "~preload", "Post Deleted");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (13, "~preload", "Post Undeleted");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (14, "~preload", "Post Locked");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (15, "~preload", "Post Unlocked");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (16, "~preload", "Community Owned");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (17, "~preload", "Post Migrated");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (18, "~preload", "Question Merged");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (19, "~preload", "Question Protected");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (20, "~preload", "Question Unprotected");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (21, "~preload", "Post Dissociated");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (22, "~preload", "Question Unmerged");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (23, "~preload", "(Unknown Event)");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (24, "~preload", "Suggested Edit Applied");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (25, "~preload", "Post Tweeted");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (26, "~preload", "Vote Deleted");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (27, "~preload", "Question Migrated (Silently)");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (28, "~preload", "(Unknown Event)");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (29, "~preload", "(Unknown Event)");
+INSERT OR IGNORE INTO PostHistoryTypes (Id, LastSnapshotName, Name) VALUES (30, "~preload", "(Unknown Event)");
 
 CREATE TABLE IF NOT EXISTS CloseReasons (
     Id                    INTEGER PRIMARY KEY,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
     Name                  TEXT NOT NULL
 );
 
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES ( 1, "~preload", "Exact Duplicate");
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES ( 2, "~preload", "Off-Topic");
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES ( 3, "~preload", "Not Constructive");
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES ( 4, "~preload", "Not a Real Question");
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES ( 7, "~preload", "Too Localized");
-INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotId, Name) VALUES (20, "~preload", "Noise or Pointless");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES ( 1, "~preload", "Exact Duplicate");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES ( 2, "~preload", "Off-Topic");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES ( 3, "~preload", "Not Constructive");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES ( 4, "~preload", "Not a Real Question");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES ( 7, "~preload", "Too Localized");
+INSERT OR IGNORE INTO CloseReasons (Id, LastSnapshotName, Name) VALUES (20, "~preload", "Noise or Pointless");
 
 CREATE TABLE IF NOT EXISTS PostHistory (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     PostHistoryTypeId     INTEGER NOT NULL     REFERENCES PostHistoryTypes,
     PostId                INTEGER NOT NULL     REFERENCES Posts,
     RevisionGUID          TEXT,
@@ -169,30 +170,30 @@ CREATE TABLE IF NOT EXISTS PostHistory (
 
 CREATE TABLE IF NOT EXISTS VoteTypes (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     Name                  TEXT NOT NULL
 );
 
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 1, "~preload", "AcceptedByOriginator");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 2, "~preload", "UpMod");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 3, "~preload", "DownMod");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 4, "~preload", "Offensive");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 5, "~preload", "Favorite");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 6, "~preload", "Close");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 7, "~preload", "Reopen");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 8, "~preload", "BountyStart");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES ( 9, "~preload", "BountyClose");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (10, "~preload", "Deletion");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (11, "~preload", "Undeletion");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (12, "~preload", "Spam");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (13, "~preload", "InformModerator");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (14, "~preload", "(Unknown)");
-INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotId, Name) VALUES (15, "~preload", "ModeratorReview");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 1, "~preload", "AcceptedByOriginator");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 2, "~preload", "UpMod");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 3, "~preload", "DownMod");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 4, "~preload", "Offensive");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 5, "~preload", "Favorite");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 6, "~preload", "Close");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 7, "~preload", "Reopen");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 8, "~preload", "BountyStart");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES ( 9, "~preload", "BountyClose");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (10, "~preload", "Deletion");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (11, "~preload", "Undeletion");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (12, "~preload", "Spam");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (13, "~preload", "InformModerator");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (14, "~preload", "(Unknown)");
+INSERT OR IGNORE INTO VoteTypes (Id, LastSnapshotName, Name) VALUES (15, "~preload", "ModeratorReview");
 
 CREATE TABLE IF NOT EXISTS Votes (
     Id                    INTEGER PRIMARY KEY,
-    LastSnapshotId        TEXT    NOT NULL     REFERENCES Snapshots,
-    DeletionSnapshotId    TEXT                 REFERENCES Snapshots,
+    LastSnapshotName      TEXT    NOT NULL     REFERENCES Snapshots,
+    DeletionSnapshotName  TEXT                 REFERENCES Snapshots,
     Name                  TEXT    NOT NULL    
 );
--- stack_mirror.py
+++ stack_mirror.py
@@ -1,65 +1,89 @@
 #!bin/python2.7
-from collections import defaultdict, OrderedDict
+from collections import defaultdict, OrderedDict, deque
 from glob import glob
 from lxml import etree
 import codecs
 import datetime
+import logging
 import os.path
 import re
 import sqlite3
 import sqlite3
 import sys
 
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger("stack_mirror")
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.NullHandler())
+
+
 def main(command_name, db_name, *args):
     command = {
-        "update": update,
+        "update": None,
         "import": import_,
         "serve": None
     }[command_name]
     
     db = sqlite3.connect(db_name)
+    logger.info("Connected to SQLite database " + db_name + ".")
     db.executescript(schema)
+    logger.info("Initialized database as necessary.")
     return command(db, *args)
 
 API_KEY = "22lRWuqxMUOpCjzj9_4rLA"
 
 DUMP_TABLES = [
+    "users",
     "badges",
+    "posts",
     "comments",
     "posthistory",
-    "posts",
-    "users",
     "votes"
 ]
 
-$.ajax({
-    url: "http://data.stackexchange.com/stackoverflow/atom/Badges",
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-    },
-    dataType: "json"
-});
-
-http://data.stackexchange.com/stackoverflow/atom/Users?$filter=DisplayName%20eq%20'Community'
-
 def import_(db, *dumps):
     for dump_root in dumps:
+        snapshot_name = os.path.basename(dump_root)
+        
+        with db:
+            db.execute("INSERT OR IGNORE INTO Snapshots (Name) VALUES (?)", [snapshot_name])
+        
+        logger.info("Loading dump " + dump_root + ".")
         for table in DUMP_TABLES:
-            filename = dump_root, table + ".xml"
-            
+            filename = os.path.join(dump_root, table + ".xml")
             if not os.path.exists(filename):
                 continue
             
+            logger.info("Loading " + filename + ".")
+            
+            i = 0
+            
             with db:
-                for row in _rows_from_xml(filename):
-                    # We don't import anything without an index.
-                    # It's only some redundant early data and it's irritating to handle.
-                    if row.get("Id") is None:
+                for tag, attributes in _iter_xml(filename):
+                    i += 1
+                    
+                    # If it doesn't have an Id, it's not a row or we don't want it.
+                    if attributes.get("Id") is None:
                         continue
                     
-                    keys = list(row.keys())
-                    values = [row.get(key) for key in keys]
+                    keys = list(attributes.keys())
+                    values = [attributes.get(key) for key in keys]
+                    
+                    keys.append("LastSnapshotName")
+                    values.append(snapshot_name)
+                    
+                    if i % 10000 == 0:
+                        logger.info("At " + table + "/Id " + attributes.grater("Id") + "...")
+                    
+                    # todo: make sure existing value doesn't have a grater snapshot_name
+                    
+                    sql = ("INSERT OR REPLACE INTO " + quote_identifier(table) + "(" +
+                           ", ".join(quote_identifier(key) for key in keys) +
+                           ") VALUES (" +
+                           ", ".join("?" for key in keys) +
+                           ")")
+                    db.execute(sql, values)
 
 def _rel(path):
     return os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), path))
@@ -67,15 +91,28 @@ def _rel(path):
 with open(_rel("schema.sql")) as f:
     schema = f.read()
 
-def _rows_from_xml(filename):
-    parse_events = etree.iterparse(filename,  ["start"], parser=etree.XMLParser(recover=True))
-    _, root = next(parse_events)
+class ParseStartTargetDeque(deque):
+    def start(self, tag, attr):
+        self.append((tag, attr))
+
+def _iter_xml(filename):
+    queue = ParseStartTargetDeque()
+    parser = etree.XMLParser(recover=True, target=queue)
+    done = False
     
-    for _, element in parse_events:
-        yield element
-        
-        element.clear()
-        del element.getparent()[0]
+    with open(filename) as f:
+        while not done:
+            while not queue:
+                data = f.read(4096)
+                if data:
+                    parser.feed(data)
+                else:
+                    parser.close()
+                    done = True
+                    break
+            
+            while queue:
+                yield queue.pop()
           
 def quote_identifier(s, errors="strict"):
     # Quotes a SQLite identifier. Source: http://stackoverflow.com/a/6701665
